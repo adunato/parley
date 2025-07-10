77e29856ef51a3bf83d3953d6ae908c90e7701ea
adunato <daniele.torrigiani@gmail.com>
2025-07-10 20:56:11 +0100

    world generator

--- src/lib/generatorPrompts.ts
+++ src/lib/generatorPrompts.ts
@@ -1,4 +1,11 @@
+const WORLD_JSON_STRUCTURE = `{
+  "world": string; // A detailed description of the world.
+}`;
+
 export const generateWorldPrompt = (worldDescription: string) => `
+You are a world-building AI for a text adventure game. Your responses MUST be a JSON object conforming to the following structure:
+${WORLD_JSON_STRUCTURE}
+
 Generate a detailed world description based on the following input. The output should be a JSON object with a single key, "world", containing a string value of the generated world description.
 
 Input World Description: ${worldDescription}
