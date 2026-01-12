842bca30898fee4f2e54aa5006f7bd6cf17c2fb9
adunato <daniele.torrigiani@gmail.com>
2026-01-12 08:07:44 +0000

    chore(test): Setup React testing environment with Jest and React Testing Library

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,20 @@ require('dotenv').config({ path: './.env.local' });
 
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/src/tests/**/*.test.ts'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: [
+    '**/src/tests/**/*.test.ts',
+    '**/src/tests/**/*.test.tsx',
+    '**/*.test.tsx'
+  ],
+  transform: {
+    '^.+\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json',
+    }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': '<rootDir>/src/tests/__mocks__/styleMock.js',
+  }
 };
